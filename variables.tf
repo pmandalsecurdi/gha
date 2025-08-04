083729cc55fb30e7902e7542c939340aec0faece
pmandalsecurdi <127840146+pmandalsecurdi@users.noreply.github.com>
2025-08-05 00:04:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,1 @@
-variable "okta_org_name" {
-  type        = string
-  description = "Okta organization name"
-}
 
-variable "okta_base_url" {
-  type        = string
-  description = "Okta base URL"
-}
-
-variable "okta_api_token" {
-  type        = string
-  description = "Okta API token"
-  sensitive   = true
-}
