45e52004bf497d1d7fd528ad922145c8e8694b26
pmandalsecurdi <127840146+pmandalsecurdi@users.noreply.github.com>
2025-08-30 17:19:31 +0530

    Update main.tf

--- modules/swa/main.tf
+++ modules/swa/main.tf
@@ -9,15 +9,15 @@ terraform {
 resource "okta_app_swa" "swa_apps" {
   for_each = { for app in var.apps : app.label => app }
 
-  label           = each.value.label
-  url             = each.value.url
-  username_field  = try(each.value.username_field, "txtbox-username")
-  password_field  = try(each.value.password_field, "txtbox-password")
-  button_field    = try(each.value.button_field, "btn-login")
-  user_name_template       = "$${source.login}"
-  user_name_template_type  = "BUILT_IN"
-  logo = "${path.root}/${each.value.logo_path}"
+  label                   = each.value.label
+  url                     = each.value.url
+  username_field          = try(each.value.username_field, "txtbox-username")
+  password_field          = try(each.value.password_field, "txtbox-password")
+  button_field            = try(each.value.button_field, "btn-login")
+  user_name_template      = "$${source.login}"
+  user_name_template_type = "BUILT_IN"
+  logo                    = "${path.root}/${each.value.logo_path}"
   lifecycle {
     prevent_destroy = true
-}
+  }
 }
