ac9de2269a0286d4b292d0b34bb553ba198e94b4
pmandalsecurdi <127840146+pmandalsecurdi@users.noreply.github.com>
2025-08-30 17:12:21 +0530

    Update main.tf

--- modules/bookmark/main.tf
+++ modules/bookmark/main.tf
@@ -11,8 +11,8 @@ resource "okta_app_bookmark" "bookmark_apps" {
 
   label = each.value.label
   url   = each.value.url
-  logo = "${path.root}/${each.value.logo_path}"
- lifecycle {
+  logo  = "${path.root}/${each.value.logo_path}"
+  lifecycle {
     prevent_destroy = true
-}
+  }
 }
