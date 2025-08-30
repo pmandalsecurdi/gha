154428785c3a952031233944503a47d047394d43
pmandalsecurdi <127840146+pmandalsecurdi@users.noreply.github.com>
2025-08-30 19:36:14 +0530

    Update main.tf

--- modules/bookmark/main.tf
+++ modules/bookmark/main.tf
@@ -12,6 +12,7 @@ resource "okta_app_bookmark" "bookmark_apps" {
   label = each.value.label
   url   = each.value.url
   logo  = "${path.root}/${each.value.logo_path}"
+  status = each.value.active == true ? "ACTIVE" : "INACTIVE"
   lifecycle {
     prevent_destroy = true
   }
